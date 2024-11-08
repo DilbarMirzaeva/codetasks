665dbeac198c18d26f471048ce21e9e66af6d49c
DilbarMirzaeva <dlbarmirza@gmail.com>
2024-11-08 14:48:24 -0800

    reverse the string and print with upper case

--- src/main/java/lesson03/ReverseAndUpperCase.java
+++ src/main/java/lesson03/ReverseAndUpperCase.java
@@ -6,12 +6,12 @@ public class ReverseAndUpperCase {
 
     public static void main(String[] args) {
 
-        Scanner sc=new Scanner(System.in);
-        String a=sc.nextLine();
-        int length=a.length();
-        String b="";
-        for(int i=length-1;i>=0;i--){
-            b+=a.charAt(i);
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        int length = a.length();
+        String b = "";
+        for (int i = length - 1; i >= 0; i--) {
+            b += a.charAt(i);
         }
         System.out.println(b.toUpperCase());
 
