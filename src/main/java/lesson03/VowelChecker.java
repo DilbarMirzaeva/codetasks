135464d797435cce7db53903b8f28555467285ae
DilbarMirzaeva <dlbarmirza@gmail.com>
2024-11-08 14:52:51 -0800

    check last character is a vowel or not

--- src/main/java/lesson03/VowelChecker.java
+++ src/main/java/lesson03/VowelChecker.java
@@ -6,12 +6,12 @@ public class VowelChecker {
 
     public static void main(String[] args) {
 
-        Scanner sc=new Scanner(System.in);
-        String a=sc.nextLine();
-        char[] arr={'a','e','i','o','u'};
-        char last=a.charAt(a.length()-1);
-        for(char c:arr){
-            if(last==c){
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        char[] arr = {'a', 'e', 'i', 'o', 'u'};
+        char last = a.charAt(a.length() - 1);
+        for (char c : arr) {
+            if (last == c) {
                 System.out.println("Last character is vowel");
                 return;
             }
