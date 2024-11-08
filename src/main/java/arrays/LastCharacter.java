5c3e27822574006427e70536382c00f5396a7a3b
DilbarMirzaeva <dlbarmirza@gmail.com>
2024-11-08 11:42:04 -0800

    LastCharacter

---
+++ src/main/java/arrays/LastCharacter.java
@@ -0,0 +1,13 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class LastCharacter {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+        System.out.println(word.charAt(word.length() - 1));
+    }
+}
