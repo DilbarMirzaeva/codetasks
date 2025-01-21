bc253b3ce57201cf09e6eda18074b31cec6f4594
DilbarMirzaeva <dlbarmirza@gmail.com>
2025-01-21 14:39:28 +0400

    OptionalSimpleTask01

--- src/main/java/optionalTask/simpleTask.java
+++ src/main/java/optionalTask/simpleTask.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 
 public class simpleTask {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        String a=sc.nextLine();
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
         printOptionalValue(a);
         printOptionalValue(null);
     }
 
-    public static void printOptionalValue(String a){
-        Optional<String> optionalValue=Optional.ofNullable(a);
-        optionalValue.ifPresentOrElse(v -> System.out.println("Value:"+v),()-> System.out.println("Empty"));
+    public static void printOptionalValue(String a) {
+        Optional<String> optionalValue = Optional.ofNullable(a);
+        optionalValue.ifPresentOrElse(v -> System.out.println("Value:" + v), () -> System.out.println("Empty"));
     }
 }
