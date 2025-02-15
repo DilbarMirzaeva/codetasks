6e86da28b0e5c615e0ef400374bee94da4e82f6d
DilbarMirzaeva <dlbarmirza@gmail.com>
2025-02-16 02:09:26 +0400

    ReflectionAPI methods check

--- src/main/java/reflectionAPI/MethodsReflectionApi.java
+++ src/main/java/reflectionAPI/MethodsReflectionApi.java
@@ -1,8 +1,6 @@
 package reflectionAPI;
 
 
-
-
 import fileIO.Student;
 
 import java.lang.reflect.Constructor;
@@ -16,68 +14,65 @@ public class MethodsReflectionApi {
     }
 
     public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException, NoSuchMethodException, InvocationTargetException, InstantiationException, ClassNotFoundException {
-        Student student1=new Student("Dilbar","Mirzayeva",18);
-        Student student2=new Student("Aida","Guliyeva",20);
+        Student student1 = new Student("Dilbar", "Mirzayeva", 18);
+        Student student2 = new Student("Aida", "Guliyeva", 20);
         System.out.println(student1);
         System.out.println(student2);
 
         System.out.println("-------------------------");
 
-        Class<?> class1=Student.class;
-        Field[] fields=class1.getDeclaredFields();
-        for(Field field:fields){
-            System.out.println(field.getName()+" :"+field.getType());
+        Class<?> class1 = Student.class;
+        Field[] fields = class1.getDeclaredFields();
+        for (Field field : fields) {
+            System.out.println(field.getName() + " :" + field.getType());
         }
 
         System.out.println("-------------------------");
 
-        Field field=class1.getDeclaredField("age");
+        Field field = class1.getDeclaredField("age");
         field.setAccessible(true);
-        field.set(student1,19);
-        System.out.println(student1.getName()+" age changed: "+student1.getAge());
+        field.set(student1, 19);
+        System.out.println(student1.getName() + " age changed: " + student1.getAge());
 
         System.out.println("-------------------------");
 
-        Method method=class1.getDeclaredMethod("print");
+        Method method = class1.getDeclaredMethod("print");
         method.invoke(student2);
 
         System.out.println("-------------------------");
 
-        Constructor<?>[] constructors=class1.getDeclaredConstructors();
-        for(Constructor<?> constructor:constructors){
-            System.out.println(constructor.getName()+":");
-            Class<?>[] typesParam=constructor.getParameterTypes();
-            for (Class<?> paramType:typesParam){
-                System.out.println("-"+paramType.getName());
+        Constructor<?>[] constructors = class1.getDeclaredConstructors();
+        for (Constructor<?> constructor : constructors) {
+            System.out.println(constructor.getName() + ":");
+            Class<?>[] typesParam = constructor.getParameterTypes();
+            for (Class<?> paramType : typesParam) {
+                System.out.println("-" + paramType.getName());
             }
         }
 
         System.out.println("-------------------------");
 
-        Constructor<?> constructor=class1.getConstructor(String.class,String.class,int.class);
-        Object newInstance=constructor.newInstance("Nigar","dcfvgbh",22);
+        Constructor<?> constructor = class1.getConstructor(String.class, String.class, int.class);
+        Object newInstance = constructor.newInstance("Nigar", "dcfvgbh", 22);
         System.out.println(newInstance);
 
         System.out.println("-------------------------");
 
-        MethodsReflectionApi obj1=new MethodsReflectionApi();
-        Method method1=MethodsReflectionApi.class.getDeclaredMethod("checkParametr", String.class);
-        method1.invoke(obj1,"Murad");
+        MethodsReflectionApi obj1 = new MethodsReflectionApi();
+        Method method1 = MethodsReflectionApi.class.getDeclaredMethod("checkParametr", String.class);
+        method1.invoke(obj1, "Murad");
 
         System.out.println("-------------------------");
 
-        Class<?> aClass=Class.forName("fileIO.Student");
-        Constructor<?> constructor2=aClass.getConstructor(String.class,int.class);
-        Object object=constructor2.newInstance("Muhammed",25);
+        Class<?> aClass = Class.forName("fileIO.Student");
+        Constructor<?> constructor2 = aClass.getConstructor(String.class, int.class);
+        Object object = constructor2.newInstance("Muhammed", 25);
         System.out.println(object);
     }
 
-    public void checkParametr(String name){
-        System.out.println("Hi,"+name);
+    public void checkParametr(String name) {
+        System.out.println("Hi," + name);
     }
-
-
-
 
 
 }
