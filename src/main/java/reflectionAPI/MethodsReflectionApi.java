66094e63a5a283184d3f09d18cf48cac58d3f45e
DilbarMirzaeva <dlbarmirza@gmail.com>
2025-02-16 02:08:15 +0400

    ReflectionAPI methods check

--- src/main/java/reflectionAPI/MethodsReflectionApi.java
+++ src/main/java/reflectionAPI/MethodsReflectionApi.java
@@ -20,22 +20,27 @@ public class MethodsReflectionApi {
         Student student2=new Student("Aida","Guliyeva",20);
         System.out.println(student1);
         System.out.println(student2);
+
         System.out.println("-------------------------");
+
         Class<?> class1=Student.class;
         Field[] fields=class1.getDeclaredFields();
         for(Field field:fields){
             System.out.println(field.getName()+" :"+field.getType());
         }
+
         System.out.println("-------------------------");
 
         Field field=class1.getDeclaredField("age");
         field.setAccessible(true);
         field.set(student1,19);
         System.out.println(student1.getName()+" age changed: "+student1.getAge());
+
         System.out.println("-------------------------");
 
         Method method=class1.getDeclaredMethod("print");
         method.invoke(student2);
+
         System.out.println("-------------------------");
 
         Constructor<?>[] constructors=class1.getDeclaredConstructors();
@@ -46,16 +51,19 @@ public class MethodsReflectionApi {
                 System.out.println("-"+paramType.getName());
             }
         }
+
         System.out.println("-------------------------");
 
         Constructor<?> constructor=class1.getConstructor(String.class,String.class,int.class);
         Object newInstance=constructor.newInstance("Nigar","dcfvgbh",22);
         System.out.println(newInstance);
+
         System.out.println("-------------------------");
 
         MethodsReflectionApi obj1=new MethodsReflectionApi();
         Method method1=MethodsReflectionApi.class.getDeclaredMethod("checkParametr", String.class);
         method1.invoke(obj1,"Murad");
+
         System.out.println("-------------------------");
 
         Class<?> aClass=Class.forName("fileIO.Student");
